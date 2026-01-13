c9a9897b8c491973406d4d4448960284fef92fe0
Ehthasham mustafa <ehthasham678@gmail.com>
2026-01-13 21:09:31 +0530

    developing stage

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        popup: 'index.html',
+        background: 'src/background/background.js',
+        content: 'src/contents/contentScript.js'
+      },
+      output: {
+        entryFileNames: '[name].js'
+      }
+    }
+  }
 })
